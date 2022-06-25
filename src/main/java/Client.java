d1f32f427df623316a5b6690ad80a7dec5aa4f5b
mohsenBagherzadehAnsari <divinehemlock@gmail.com>
2022-06-25 12:40:07 +0430

    made client code more readable

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,27 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
-    public static void main(String[] args) throws IOException {
+public class Client
+{
+    public static void main(String[] args) throws IOException
+    {
         String host = "127.0.0.1";
         int port = 32000;
-        try (Socket socket = new Socket(host, port)){
+        try (Socket socket = new Socket(host, port))
+        {
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             Scanner scanner = new Scanner(System.in);
             String line = null;
-            while (!"exit".equalsIgnoreCase(line)) {
+            while (!"exit".equalsIgnoreCase(line))
+            {
                 line = scanner.nextLine();
                 out.println(line);
                 out.flush();
                 System.out.println("Server replied " + in.readLine());
             }
             scanner.close();
-        } catch (IOException e){
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
